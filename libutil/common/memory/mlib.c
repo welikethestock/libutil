5b94868b9208a6d25bbfee81c5bcc55e5970e11f
welikethestock <>
2023-06-02 13:22:42 +0200

    memcpy update

--- libutil/common/memory/mlib.c
+++ libutil/common/memory/mlib.c
@@ -1,6 +1,5 @@
 #include "mlib.h"
 
-// TODO: vectorization
 LIBUTIL_API
 void *LibUtil_Memcpy(void *Destination, const void *Source, libutil_size Length)
 {
@@ -49,42 +48,95 @@ void *LibUtil_Memcpy(void *Destination, const void *Source, libutil_size Length)
 
                 Copied = 16;
             }
+
+            _Destination += Copied;
+            _Source += Copied;
+            Length -= Copied;
         }
     #endif
     #if defined(LIBUTIL_FEATURE_SSE2) || defined(LIBUTIL_FEATURE_AVX) || defined(LIBUTIL_FEATURE_AVX512F)
         else {
     #endif
-            if(Length >= 8)
+            if(Length >= (16 + 1)) // align address
             {
-                *(long long *)(_Destination) = *(long long *)(_Source);
+                libutil_size Align = (libutil_size)(_Destination) & (16 - 1);
 
-                Copied = 8;
-            }
-            else if(Length >= 4)
-            {
-                *(int *)(_Destination) = *(int *)(_Source);
+                if(Align >= 8)
+                {
+                    *(libutil_i64 *)(_Destination) = *(libutil_i64 *)(_Source);
 
-                Copied = 4;
-            }
-            else if(Length >= 2)
-            {
-                *(short *)(_Destination) = *(short *)(_Source);
+                    _Destination += 8;
+                    _Source += 8;
+                    Length -= 8;
+                    Align -= 8;
+                }
 
-                Copied = 2;
+                if(Align >= 4)
+                {
+                    *(libutil_i32 *)(_Destination) = *(libutil_i32 *)(_Source);
+
+                    _Destination += 4;
+                    _Source += 4;
+                    Length -= 4;
+                    Align -= 4;
+                }
+
+                if(Align >= 2)
+                {
+                    *(libutil_i16 *)(_Destination) = *(libutil_i16 *)(_Source);
+
+                    _Destination += 2;
+                    _Source += 2;
+                    Length -= 2;
+                    Align -= 2;
+                }
+                if(Align >= 1)
+                {
+                    *_Destination = *_Source;
+
+                    _Destination += 1;
+                    _Source += 1;
+                    Length -= 1;
+                    Align -= 1;
+                }
             }
             else
             {
-                *_Destination = *_Source;
+                while(Length != 0)
+                {
+                    if(Length >= 8)
+                    {
+                        *(libutil_i64 *)(_Destination) = *(libutil_i64 *)(_Source);
 
-                Copied = 1;
+                        Copied = 8;
+                    }
+                    else if(Length >= 4)
+                    {
+                        *(libutil_i32 *)(_Destination) = *(libutil_i32 *)(_Source);
+
+                        Copied = 4;
+                    }
+                    else if(Length >= 2)
+                    {
+                        *(libutil_i16 *)(_Destination) = *(libutil_i16 *)(_Source);
+
+                        Copied = 2;
+                    }
+                    else
+                    {
+                        *_Destination = *_Source;
+
+                        Copied = 1;
+                    }
+
+                    _Destination += Copied;
+                    _Source += Copied;
+                    Length -= Copied;
+                }
             }
     #if defined(LIBUTIL_FEATURE_SSE2) || defined(LIBUTIL_FEATURE_AVX) || defined(LIBUTIL_FEATURE_AVX512F)
         }
     #endif
-
-        _Destination += Copied;
-        _Source += Copied;
-        Length -= Copied;
     }
 
     return Destination;
