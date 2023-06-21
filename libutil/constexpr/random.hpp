3603647ab53660f83b4343395ec6e2b4d11504df
welikethestock <>
2023-06-21 12:31:54 +0200

    fix

--- libutil/constexpr/random.hpp
+++ libutil/constexpr/random.hpp
@@ -6,13 +6,10 @@
 namespace LibUtil
 {
     template< typename _T, LibUtil::U32 Seed, LibUtil::U32 Counter >
-    constexpr _T CompileTimeRandom();
-}
-
-template< typename _T, libutil_u32 Seed, libutil_u32 Counter >
-constexpr libutil_u32 CompileTimeRandom()
-{
-    return (Seed + Counter) * 1664525 + 1013904223;
+    constexpr _T CompileTimeRandom()
+    {
+        return (Seed + Counter) * 1664525 + 1013904223;
+    }
 }
 
 #endif
